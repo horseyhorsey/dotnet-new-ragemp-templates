6687c8e6ccf568cf9a0c60289d0351b91b2356cc
dave <dub.cyclone@gmail.com>
2018-12-27 18:54:59 +0000

    Fix Client project in rage-solution missing quote end in build events. Added Client Resource project to house client scripts.

---
+++ src/Company.RageMpClientResource.md
@@ -0,0 +1,26 @@
+# Client Resource Project
+
+---
+
+##	dotnet new rage-clientproj --help
+---
+
+
+## dotnet new rage-clientproj
+
+This template creates a RAGE-MP client resource project (defaulted to netcoreapp2.1). 
+
+***Use -n to specify name for the folder and resource to create.***
+
+	dotnet new rage-clientproj -n BeastClientResourceProject
+
+With directory other than C:/RAGEMP
+
+	dotnet new rage-clientproj -r "E:/RAGEMP/" -n BeastClientResourceProject
+
+
+
+
+
+
+
---
+++ src/content/Company.RageMpClientResource/MyFirstScript.cs
@@ -0,0 +1,29 @@
+using System;
+
+using RAGE;
+using RAGE.Elements;
+
+public class MyFirstScript : Events.Script
+{
+    public MyFirstScript()
+    {
+        RAGE.Chat.Output("Loaded MyFirstScript Script");
+        RAGE.Events.OnPlayerCommand += OnPlayerCommand;  
+    }
+
+    public void OnPlayerCommand(string cmd, RAGE.Events.CancelEventArgs cancel)
+    {
+        Chat.Output($"OnPlayerCommand: {cmd}");
+        if (cmd == "heal")
+        {
+            Chat.Output($"Healing");
+            RAGE.Elements.Player.LocalPlayer.SetHealth(200);
+            Chat.Output($"PlayerHealth: {Player.LocalPlayer.GetHealth()}");
+        }
+        else if (cmd == "armour")
+        {
+            Chat.Output($"Armour");
+            RAGE.Elements.Player.LocalPlayer.SetArmour(100);
+        }
+    }
+}
